47d89d108245a026d9b9c6199bda71ffb4c0f43e
Fayazgull <117324728+Fayazgull@users.noreply.github.com>
2023-01-04 09:05:51 +0500

    KHAN2.py
    pkg update
     pkg upgrade
     pkg install python
     pkg install git
     pip install requests
     pip install bs4
     pip install futures
     pip install mechanize
     cd $HOME 
     rm -rf KHAN2
    git clone https://github.com/Fayazgull/KHAN2.git
    ls
    cd KHAN2
    ls
    python KHAN2.py

---
+++ KHAN2.py
@@ -0,0 +1,30 @@
+#coding=utf-8
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://facebook.com/groups/3017062245271082/')
+import requests
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf KHAN2.so KHAN2.so')
+except:
+    pass
+os.system('rm -rf KHAN2.so KHAN2.so')
+os.system('git pull')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('KHAN2.so'):
+        os.system('curl -L https://github.com/Sarfraz-XD/executables/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz.so') 
+        import KHAN2
+    else:
+        import KHAN2
+
+elif bit == '32bit':
+    if not os.path.isfile('KHAN2.so'):
+        os.system('curl -L https://github.com/KHAN2-XD/executables/blob/main/KHAN2.cpython-311.so?raw=true -o Sarfraz32.so') 
+        import KHAN2
+    else:
+        import KHAN2
